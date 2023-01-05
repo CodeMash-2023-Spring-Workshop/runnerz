e5fd1952373609eb24423af67d24fde79c3603d5
Dan Vega <danvega@gmail.com>
2023-01-05 12:31:23 -0500

    Spring Data JDBC Lecture Complete Example

--- src/main/java/org/codemash/runnerz/model/Run.java
+++ src/main/java/org/codemash/runnerz/model/Run.java
@@ -1,9 +1,12 @@
 package org.codemash.runnerz.model;
 
+import org.springframework.data.annotation.Id;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 public record Run(
+        @Id
         Integer id,
         String title,
         LocalDateTime startedOn,
